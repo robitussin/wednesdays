9f9dec594ba33d5e2f2a82dcfc029ceb5233dce4
robitussin <elizerponiojr@gmail.com>
2023-06-24 00:11:16 +0800

    added hello Liyah

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -77,7 +77,7 @@ public class LoginController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // TODO Auto-generated method stub
-
+        // Hello Liyah!
     }
 
     public void login(ActionEvent event) throws IOException {
